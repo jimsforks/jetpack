df49e21774d88ee4d82c56a37a562a9a15276758
Andrew Kane <andrew@chartkick.com>
2020-05-26 00:38:32 -0700

    Just init once [skip ci]

--- R/load.R
+++ R/load.R
@@ -3,25 +3,24 @@
 #' @export
 #' @keywords internal
 load <- function() {
-  dir <- findDir(getwd())
+  wd <- getwd()
+  dir <- findDir(wd)
 
   if (is.null(dir)) {
     stopNotPackified()
   }
+
+  # TODO remove these
+  library(desc)
+  library(docopt)
 
   venv_dir <- setupEnv(dir)
 
   # must source from virtualenv directory
   # for RStudio for work properly
   # this should probably be fixed in Packrat
-  wd <- getwd()
   tryCatch({
     setwd(venv_dir)
-
-    # TODO remove these
-    library(desc)
-    library(docopt)
-
     quietly(source("renv/activate.R"))
   }, finally={
     setwd(wd)
