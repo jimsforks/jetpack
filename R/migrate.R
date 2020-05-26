cbef4203994692bc298abee0b80b8c7e69669e6e
Andrew Kane <andrew@chartkick.com>
2020-05-26 02:56:08 -0700

    Better migrate [skip ci]

--- R/migrate.R
+++ R/migrate.R
@@ -11,16 +11,16 @@ migrate <- function() {
 
     dest <- file.path(dir, "renv.lock")
     if (file.exists(dest)) {
-      stop("renv.lock already exists.")
+      message("renv.lock already exists. You should be good to go.")
+    } else {
+      temp_dir <- tempDir()
+      packrat_dir <- file.path(temp_dir, "packrat")
+      dir.create(packrat_dir)
+      file.copy(file.path(dir, "packrat.lock"), file.path(packrat_dir, "packrat.lock"))
+      quietly(renv::migrate(project=temp_dir, packrat=c("lockfile")))
+      file.copy(file.path(temp_dir, "renv.lock"), dest)
+      cmd <- if (!interactive()) "jetpack install" else "jetpack::install()"
+      message(paste0("Lockfile migration successful! To finish migrating:\n1. Delete packrat.lock\n2. Run '", cmd, "'"))
     }
-
-    temp_dir <- tempDir()
-    packrat_dir <- file.path(temp_dir, "packrat")
-    dir.create(packrat_dir)
-    file.copy(file.path(dir, "packrat.lock"), file.path(packrat_dir, "packrat.lock"))
-    quietly(renv::migrate(project=temp_dir, packrat=c("lockfile")))
-    file.copy(file.path(temp_dir, "renv.lock"), dest)
-    cmd <- if (!interactive()) "jetpack install" else "jetpack::install()"
-    success(paste0("Lockfile migration successful! To finish migrating:\n1. Delete packrat.lock\n2. Run '", cmd, "'"))
   })
 }
